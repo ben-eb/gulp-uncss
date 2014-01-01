d495ab2a47925d5138d5251659372476159f6412
Ben Briggs <therealbenbriggs@hotmail.com>
2014-01-01 01:13:27 +0000

    event stream -> map stream

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var es = require('event-stream');
+var map = require('map-stream');
 var uncss = require('uncss');
 
 module.exports = function() {
@@ -6,7 +6,7 @@ module.exports = function() {
     var html = arguments[0].html;
     var ignore = arguments[0].ignore;
     var timeout = arguments[0].timeout;
-    return es.map(function(file, cb) {
+    return map(function(file, cb) {
         uncss(html, { raw: String(file.contents), compress: false, ignore: ignore, timeout: timeout }, function(output) {
             file.contents = new Buffer(output);
             cb(null, file);
